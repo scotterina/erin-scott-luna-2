580f33ca9d2dc5e9a0c302f9ac162558f8605fc3
Erin Scott <212621843+scotterina@users.noreply.github.com>
2025-10-10 22:27:08 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -27,3 +27,39 @@ for (let i = 0; i < skills.length; i++) {
   skill.innerText = skills[i];
   skillsList.appendChild(skill);
 }
+//---------- Message Form ---------
+
+const messageForm = document.querySelector("form[name='leave_message']");
+
+messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const userName = event.target.usersName.value;
+  const userEmail = event.target.usersEmail.value;
+  const userMessage = event.target.usersMessage.value;
+
+  console.log("Name: ", userName);
+  console.log("Email: ", userEmail);
+  console.log("Message: ", userMessage);
+
+  const messageSection = document.getElementById("My Messages");
+  const messageList = messageSection.querySelector("ul");
+  const newMessage = document.createElement("li");
+
+  newMessage.innerHTML = `<a href="mailto:${userEmail}">${userName}</a>: <span>${userMessage}</span>`;
+
+  const removeButton = document.createElement("button");
+  removeButton.innerHTML = "remove";
+  removeButton.className = "remove-btn";
+  removeButton.type = "button";
+
+  removeButton.addEventListener("click", function () {
+    const entry = removeButton.parentNode;
+    entry.remove();
+  });
+
+  newMessage.appendChild(removeButton);
+  messageList.appendChild(newMessage);
+
+  messageForm.reset();
+});
